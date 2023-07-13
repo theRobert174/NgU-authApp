5baa390e008c50bc75764585d70dc4b39b938061
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-12 20:33:35 -0600

    react to changed auth

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -6,10 +6,11 @@ import { AuthStatus } from '../interfaces';
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
 
   const authService = inject(AuthService);
+  const router = inject(Router);
 
+  if( authService.authStatus() === AuthStatus.checking ) return false;
   if( authService.authStatus() === AuthStatus.authenticated ) return true;
 
-  const router = inject(Router);
 
   router.navigateByUrl('/auth/login');
 
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   public currentUser = computed( () => this._currentUser() );
   public authStatus = computed( () => this._authStatus() );
 
-  constructor() { }
+  constructor() { this.checkAuthStatus().subscribe(); }
 
   private setAuthentication(user: User, token: string) : boolean {
     this._currentUser.set(user);
